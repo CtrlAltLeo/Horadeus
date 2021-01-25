933e8a9aa521890e72e3fb1d939868c46b0a8c69
Leo D <leodevick@gmail.com>
2021-01-25 15:41:56 -0600

    Working basic text Box

--- TextEngine/diobox.gd
+++ TextEngine/diobox.gd
@@ -1,16 +1,59 @@
 extends Node
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+#array containing all the text. Set up when a new Diobox is made
+var text = ["Dang, daniel.","I've already got a basic text engine","adding choices is next!"]
+onready var textBox = $textBox
+onready var textBoxText = $text
+
+#which text line from text array is being used
+var active = 0
+
+#if all avalible text has been printed 
+var endOfLine = false
+
+var canSee = 0
+onready var canSeeTimer = $canSeeTimer
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	
+	textBoxText.text = text[active]
+	canSeeTimer.start()
+	
+	
+	
+	
+# warning-ignore:unused_argument
+func _process(delta):
+	textBoxText.visible_characters = canSee
+	
+	if Input.is_action_just_pressed("leftMouse") and !endOfLine:
+		canSee = text[active].length()
+	if Input.is_action_just_pressed("leftMouse") and endOfLine:
+		IsLastLine()
+		
+	
+	if canSee >= text[active].length():
+		endOfLine = true
+	else:
+		endOfLine = false
+	
+func IsLastLine():
+	if active + 1 < text.size():
+		nextPage()
+	else:
+		self.queue_free()
 
+func nextPage():
+	canSee = 0
+	endOfLine = false
+	active += 1
+	textBoxText.text = text[active]
+	canSeeTimer.start()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_canSeeTimer_timeout():
+	if !endOfLine:
+		canSee += 1
+		canSeeTimer.start()
+		print(canSee)
