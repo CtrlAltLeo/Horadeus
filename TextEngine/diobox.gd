39c96b7dc36892cf236ff4b4869bbd85b8cb658a
Leo D <leodevick@gmail.com>
2021-01-25 17:23:12 -0600

    Choices for DioBOx

--- TextEngine/diobox.gd
+++ TextEngine/diobox.gd
@@ -1,5 +1,8 @@
 extends Node
 
+signal endBox(active)
+signal nextPage(active)
+
 
 #array containing all the text. Set up when a new Diobox is made
 var text = ["Dang, daniel.","I've already got a basic text engine","adding choices is next!"]
@@ -12,11 +15,26 @@ var active = 0
 #if all avalible text has been printed 
 var endOfLine = false
 
+#how many characters a player can see. Makes the "scrolling text" effect 
 var canSee = 0
 onready var canSeeTimer = $canSeeTimer
 
+#All lines of text that will terminate the box
+var ends = []
+
+#If active is a choice
+var isAChoice = false
+
+var choiceTriggers = []
+
+#The actual Choice Text
+#Choice Format When It Triggers:How Many there are:ChoiceText1-X:Corrisponding Choice Destination
+var choices = ["1:2:Yes:No:2:3","3:4:3:6"]
 
 func _ready():
+	
+	initChoices()
+	checkIfChoice()
 	
 	textBoxText.text = text[active]
 	canSeeTimer.start()
@@ -28,27 +46,35 @@ func _ready():
 func _process(delta):
 	textBoxText.visible_characters = canSee
 	
-	if Input.is_action_just_pressed("leftMouse") and !endOfLine:
-		canSee = text[active].length()
-	if Input.is_action_just_pressed("leftMouse") and endOfLine:
-		IsLastLine()
-		
-	
 	if canSee >= text[active].length():
 		endOfLine = true
 	else:
 		endOfLine = false
 	
 func IsLastLine():
-	if active + 1 < text.size():
+	if active + 1 < text.size() and !checkActiveAgainstEnd():
 		nextPage()
 	else:
+		emit_signal("endBox", active)
 		self.queue_free()
 
+#loops through all the ends, and will return a True to isLastLine to terminate Text Box
+func checkActiveAgainstEnd():
+	if ends.size() != 0:
+		for i in ends:
+			if active == i:
+				return true
+			else:
+				pass
+		
+	
+
 func nextPage():
+	
 	canSee = 0
 	endOfLine = false
 	active += 1
+	emit_signal("nextPage",active)
 	textBoxText.text = text[active]
 	canSeeTimer.start()
 
@@ -56,4 +82,27 @@ func _on_canSeeTimer_timeout():
 	if !endOfLine:
 		canSee += 1
 		canSeeTimer.start()
-		print(canSee)
+		#print(canSee)
+
+#Determines MouseClick in Box
+func _on_clickableObject_clicked():
+	
+	if !endOfLine:
+		canSee = text[active].length()
+	if endOfLine and !isAChoice:
+		IsLastLine()
+		
+func initChoices():
+	for i in choices:
+		print(i)
+		var choiceStaging = i.split(":")
+		print(choiceStaging[0])
+		choiceTriggers.append(choiceStaging[0])
+		
+func checkIfChoice():
+	for i in choiceTriggers:
+		if active == i:
+			isAChoice = true
+		
+				
+		
