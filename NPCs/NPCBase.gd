ca0345166c2824a0b44f2491be2e97ab633b8d0e
Leo D <leodevick@gmail.com>
2021-01-29 13:42:55 -0600

    better 3d

--- NPCs/NPCBase.gd
+++ NPCs/NPCBase.gd
@@ -1,5 +1,6 @@
 extends AnimatedSprite3D
 
+onready var TargetPos = get_parent().get_child(0).translation
 
 var NW = Vector3(-1,0,-1)
 
@@ -9,9 +10,22 @@ var SW = Vector3(-1,0,1)
 
 var SE = Vector3(1,0,1)
 
+var buffer = 5
+
 #0 is front, 1 is side, 2 is back
 var NPC_Facing = 0
 
+var formula = Vector3()
+
 func _ready():
 	
-	print(NW<NE)
+	
+	print(TargetPos)
+	
+	if TargetPos.z < self.translation.z and (TargetPos.x > translation.x - 5 and TargetPos.x < translation.x + 5 ):
+		print("Front")
+	if TargetPos.z > self.translation.z:
+		print("Back")
+		
+	if TargetPos.x > translation.x + buffer:
+		print("side")
